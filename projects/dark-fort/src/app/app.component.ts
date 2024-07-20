05b7f0d8925e52f6d54150abdd39e96da39e2479
labak <labak.george@gmail.com>
2024-07-21 01:30:32 +0300

    feat(dark-fort): scaffolded score component

--- projects/dark-fort/src/app/app.component.ts
+++ projects/dark-fort/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {RoomMapComponent} from './components/room-map/room-map.component';
 import {CharacterComponent} from './components/character/character.component';
 import {StateService} from './services/state.service';
+import {ScoreComponent} from './components/score/score.component';
 
 @Component({
   selector: 'dark-fort-root',
   standalone: true,
-  imports: [RouterOutlet, RoomMapComponent, CharacterComponent],
+  imports: [RouterOutlet, RoomMapComponent, CharacterComponent, ScoreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ projects/dark-fort/src/app/components/score/score.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dark-fort-score',
+  standalone: true,
+  imports: [],
+  templateUrl: './score.component.html',
+  styleUrl: './score.component.css'
+})
+export class ScoreComponent {
+
+}
