54d20db7420871e4d556a0479bb6cdbc9f534ce1
labak <labak.george@gmail.com>
2024-07-20 21:53:34 +0300

    feat(dark-fort): removed unused methods

--- projects/dark-fort/src/app/services/room.service.ts
+++ projects/dark-fort/src/app/services/room.service.ts
@@ -24,19 +24,6 @@ export class RoomService {
   private readonly _mapWidth: number;
   private readonly _mapHeight: number;
 
-  private getOppositeExit(exit: Cardinality): Cardinality {
-    switch (exit) {
-      case Cardinality.north:
-        return Cardinality.south;
-      case Cardinality.east:
-        return Cardinality.west;
-      case Cardinality.south:
-        return Cardinality.north;
-      case Cardinality.west:
-        return Cardinality.east;
-    }
-  }
-
   generateRandomRoom(x: number, y: number, existingExits: Cardinality[] = []): Room {
 
     const roomExits = new Set(existingExits);
@@ -126,48 +113,5 @@ export class RoomService {
     }
 
     return Cardinality.south;
-  }
-
-  generateRandomMap(rows: number, cols: number): Room[] {
-    const map: Room[] = [];
-
-    for (let r = 0; r < rows; r++) {
-      for (let c = 0; c < cols; c++) {
-        const existingExits: Cardinality[] = [];
-
-        if (r > 0) {
-          const northRoom = map.find(room => room.x === r - 1 && room.y === c);
-          if (northRoom?.exits.includes(Cardinality.south)) {
-            existingExits.push(Cardinality.north);
-          }
-        }
-
-        if (c > 0) {
-          const westRoom = map.find(room => room.x === r && room.y === c - 1);
-          if (westRoom?.exits.includes(Cardinality.east)) {
-            existingExits.push(Cardinality.west);
-          }
-        }
-
-        const room = this.generateRandomRoom(r, c, existingExits);
-        map.push(room);
-
-        if (r > 0) {
-          const northRoom = map.find(room => room.x === r - 1 && room.y === c);
-          if (room.exits.includes(Cardinality.north) && !northRoom?.exits.includes(Cardinality.south)) {
-            northRoom?.exits.push(Cardinality.south);
-          }
-        }
-
-        if (c > 0) {
-          const westRoom = map.find(room => room.x === r && room.y === c - 1);
-          if (room.exits.includes(Cardinality.west) && !westRoom?.exits.includes(Cardinality.east)) {
-            westRoom?.exits.push(Cardinality.east);
-          }
-        }
-      }
-    }
-
-    return map;
   }
 }
