2b7d6dd92b76ebd40e901440247194a7a78923e2
labak <labak.george@gmail.com>
2024-08-01 14:08:20 +0300

    feat(dark-fort): partial implementation of riddle room !DOES NOT  BUILD

--- projects/dark-fort/src/app/components/rooms/riddle-room/riddle-room.component.ts
+++ projects/dark-fort/src/app/components/rooms/riddle-room/riddle-room.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {StateService} from '../../../services/state.service';
+import {MatButton} from '@angular/material/button';
+import {MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle} from '@angular/material/dialog';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'dark-fort-riddle-room',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogTitle,
+    NgIf
+  ],
   templateUrl: './riddle-room.component.html',
   styleUrl: './riddle-room.component.css'
 })
 export class RiddleRoomComponent {
+  solveRoll!: number;
+  damageRoll!: number;
 
+  constructor(public stateService: StateService) {
+    this.solveRoll = this.stateService.calculateTrap();
+    this.damageRoll = this.stateService.calculateDamage(4, 0, true);
+  }
 }
