f837d087abd875fa5607ef3f46a66ee1b1da9e21
George Labakakis <labak.george@gmail.com>
2024-08-09 18:17:44 +0300

    feat(dark-fort): implemented level up conditions

--- projects/dark-fort/src/app/components/score/score.component.ts
+++ projects/dark-fort/src/app/components/score/score.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import {StateService} from '../../services/state.service';
+import {MatButton} from "@angular/material/button";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'dark-fort-score',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    NgIf
+  ],
   templateUrl: './score.component.html',
   styleUrl: './score.component.css'
 })
 export class ScoreComponent {
 
   constructor(public stateService: StateService) {
+  }
+
+  canLevelPoints(): boolean {
+    return this.stateService.calculateExploredRoomsCount() >= 12 && this.stateService.character.points >= 15;
+  }
+
+  canLevelSilver(): boolean {
+    return this.stateService.character.silver > 40;
   }
 }
