8c61266b8df8e959514ed61da7582c8117e56ff7
labak <labak.george@gmail.com>
2024-08-15 02:37:49 +0300

    feat(common): implemented dice dialog

---
+++ projects/common/src/lib/models/DiceDialog.ts
@@ -0,0 +1,6 @@
+export interface DiceDialog {
+  data: {
+    results: number[];
+    sides: number;
+  };
+}
--- projects/common/src/lib/services/dice.service.ts
+++ projects/common/src/lib/services/dice.service.ts
@@ -1,11 +1,13 @@
-import {Injectable} from '@angular/core';
+import {Injectable, Type} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {DiceDialog} from '../models/DiceDialog';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DiceService {
 
-  constructor() {
+  constructor(private dialog: MatDialog) {
   }
 
   rollAndSumDice(numDice: number, numSides: number): number {
@@ -22,5 +24,47 @@ export class DiceService {
     }
     const randomIndex = Math.floor(Math.random() * array.length);
     return array[randomIndex];
+  }
+
+  async rollDiceWithConfirmation<T extends DiceDialog>(numberOfDice: number, sides: number, dialogComponent: Type<T>): Promise<number[]> {
+    let results: number[] = this.rollDice(numberOfDice, sides);
+    let confirmed = false;
+
+    while (!confirmed) {
+      const dialogRef = this.dialog.open(dialogComponent, {
+        width: '250px',
+        data: {results, sides}
+      });
+
+      const result = await dialogRef.afterClosed().toPromise();
+
+      if (result === 'accept') {
+        confirmed = true;
+      } else {
+        results = this.rollDice(numberOfDice, sides);
+      }
+    }
+
+    return results;
+  }
+
+  async rollAndSumDiceWithConfirmation <T extends DiceDialog>(numberOfDice: number, sides: number, dialogComponent: Type<T>): Promise<number> {
+    const results = await this.rollDiceWithConfirmation(numberOfDice, sides, dialogComponent);
+    return results.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  }
+
+  private rollDice(numberOfDice: number, sides: number): number[] {
+    let results: number[] = [];
+
+    for (let i = 0; i < numberOfDice; i++) {
+      let rollResult = this.rollDie(sides);
+      results.push(rollResult);
+    }
+
+    return results;
+  }
+
+  private rollDie(sides: number): number {
+    return Math.floor(Math.random() * sides) + 1;
   }
 }
