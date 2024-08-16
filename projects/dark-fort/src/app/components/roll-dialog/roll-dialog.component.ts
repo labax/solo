196601c5cd8ec032b3714ba9db6397b4dc5a6775
labak <labak.george@gmail.com>
2024-08-16 18:49:43 +0300

    feat(dark-fort): implemented roll auto close

--- projects/dark-fort/src/app/components/roll-dialog/roll-dialog.component.ts
+++ projects/dark-fort/src/app/components/roll-dialog/roll-dialog.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {DiceDialog} from '../../../../../common/src/lib/models/DiceDialog';
 import {
   MAT_DIALOG_DATA,
@@ -8,6 +8,8 @@ import {
   MatDialogTitle
 } from '@angular/material/dialog';
 import {MatButton} from '@angular/material/button';
+import {StateService} from '../../services/state.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'dark-fort-roll-dialog',
@@ -16,23 +18,35 @@ import {MatButton} from '@angular/material/button';
     MatDialogTitle,
     MatDialogContent,
     MatDialogActions,
-    MatButton
+    MatButton,
+    NgIf
   ],
   templateUrl: './roll-dialog.component.html',
   styleUrl: './roll-dialog.component.css'
 })
-export class RollDialogComponent implements DiceDialog {
+export class RollDialogComponent implements DiceDialog, OnInit {
   constructor(
+    private stateService: StateService,
     public dialogRef: MatDialogRef<RollDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { results: number[], sides: number, description: string }
-  ) {}
+  ) {
+  }
+
+  ngOnInit(): void {
+    if(!this.canReroll()) {
+      setTimeout(() => this.dialogRef.close('accept'), 3000);
+    }
+  }
+
+  canReroll(): boolean {
+    return this.stateService.hasItem('omen');
+  }
 
   reroll(): void {
     this.dialogRef.close('reroll');
   }
 
-  accept(): void {
+  accept() {
     this.dialogRef.close('accept');
   }
-
 }
--- projects/dark-fort/src/app/services/state.service.ts
+++ projects/dark-fort/src/app/services/state.service.ts
@@ -183,6 +183,9 @@ export class StateService {
   }
 
   hasItem(key: ItemIdentifier): boolean {
+    if(!this.character?.inventory) {
+      return false;
+    }
     return !!this.character.inventory.find(item => item.id === key);
   }
 
