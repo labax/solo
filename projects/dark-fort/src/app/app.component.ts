f26ab11c597eae5dccc95bb479ae0182bdcb8efb
George Labakakis <labak.george@gmail.com>
2024-08-23 12:33:51 +0300

    feat(dark-fort): added and registered combat icons

--- projects/dark-fort/src/app/app.component.ts
+++ projects/dark-fort/src/app/app.component.ts
@@ -44,7 +44,19 @@ export class AppComponent {
       .addSvgIcon('sorcerer', this.setPath('monsters/sorcerer.svg'))
       .addSvgIcon('troll', this.setPath('monsters/troll.svg'))
       .addSvgIcon('heart-empty', this.setPath('heart-red-outline.svg'))
-      .addSvgIcon('heart-full', this.setPath('heart-red.svg'));
+      .addSvgIcon('heart-full', this.setPath('heart-red.svg'))
+      .addSvgIcon('cloak', this.setPath('combat/cloak.svg'))
+      .addSvgIcon('daemon', this.setPath('combat/daemon.svg'))
+      .addSvgIcon('potion', this.setPath('combat/potion.svg'))
+      .addSvgIcon('aegis', this.setPath('scrolls/aegis.svg'))
+      .addSvgIcon('palms', this.setPath('scrolls/palms.svg'))
+      .addSvgIcon('summon', this.setPath('scrolls/summon.svg'))
+      .addSvgIcon('dagger', this.setPath('weapons/dagger.svg'))
+      .addSvgIcon('flail', this.setPath('weapons/flail.svg'))
+      .addSvgIcon('sword', this.setPath('weapons/sword.svg'))
+      .addSvgIcon('warHammer', this.setPath('weapons/warHammer.svg'))
+      .addSvgIcon('zweihander', this.setPath('weapons/zweihander.svg'));
+
 
     this.stateService.initialize();
   }
